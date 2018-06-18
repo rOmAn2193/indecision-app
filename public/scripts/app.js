4658217c1113df61cc34edd1235a4828b2dcdf0e
Roman Puzey <roman_puzey@aol.com>
2018-06-18 13:31:59 -0600

    first commmit

---
+++ public/scripts/app.js
@@ -0,0 +1,51 @@
+'use strict';
+
+console.log('App.js is running!');
+
+// JSX - JavaScript XML
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Indecision App'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some info'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    )
+  )
+);
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Andrew Mead'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 24'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: New York'
+  )
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(templateTwo, appRoot);
---
+++ src/app.js
@@ -0,0 +1,23 @@
+console.log('App.js is running!');
+
+// JSX - JavaScript XML
+var template = (
+  <div>
+    <h1>Indecision App</h1> 
+    <p>This is some info</p>
+    <ol>
+        <li>Item one</li>
+    </ol>
+  </div>
+);
+
+var templateTwo = (
+    <div>
+      <h1>Andrew Mead</h1>
+      <p>Age: 24</p>
+      <p>Location: New York</p>
+    </div>
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(templateTwo, appRoot);
