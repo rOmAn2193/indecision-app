86cf1a8a95eec5dd2b99d746f710d859d1dd610d
Roman Puzey <romanpuzey@aol.com>
2018-07-09 14:02:43 -0600

    added events & attributes

--- src/app.js
+++ src/app.js
@@ -1,48 +1,41 @@
 console.log('App.js is running!');
 
-// JSX - JavaScript XML
-// logical and operator 
-
-var app = {
-    title: 'Javascript expressions',
-    subtitle: 'This is my subtitle',
-    options: ['One', 'Two'],
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
 };
-
-var template = (
+const template = (
   <div>
-    <h1>{app.title}</h1> 
+    <h1>{app.title}</h1>
     {app.subtitle && <p>{app.subtitle}</p>}
     <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
     <ol>
-        <li>Item one</li>
-        <li>Item two</li>
+      <li>Item one</li>
+      <li>Item two</li>
     </ol>
   </div>
 );
 
-var user = {
-    name: 'Brian',
-    age: 28,
-    location: 'Hawaii'
+let count = 0;
+const addOne = () => {
+  console.log('addOne');
 };
-var userName = 'Alex';
-var userAge = 27;
-var userLocation = 'Los Angeles';
+const minusOne = () => {
+  console.log('minusOne');
+};
+const reset = () => {
+  console.log('reset');
+};
+const templateTwo = (
+  <div>
+    <h1>Count: {count}</h1>
+    <button onClick={addOne}>+1</button>
+    <button onClick={minusOne}>-1</button>
+    <button onClick={reset}>reset</button>
+  </div>
+);
 
-function getLocation(location) { 
-    if (location) {
-        return <p>Location: {location}</p>;
-    } 
-}
-
-var templateTwo = (
-    <div>
-      <h1>{user.name ? user.name : 'Anonymous'}</h1>
-      {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-      {getLocation(user.location)}
-    </div>
-); 
-var appRoot = document.getElementById('app');
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(templateTwo, appRoot);
